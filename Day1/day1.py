005938cd7d245e4488a32c28740d19f3d32b8409
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-12-01 17:26:44 +0100

    Day1 finish

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,36 +2,31 @@ years_list = []
 new_list = []
 file = "expanses.txt"
 with open(file, "r") as f:
-    for line in f:
-        year = int(f.readline())
-        years_list.append(year)
-        new_list.append(2020 - year)
+    years_list = f.readlines()
+    idx = 0
+    for elem in years_list:
+        years_list[idx] = int(elem.rstrip("\n"))
+        idx += 1
+        new_list.append(2020 - int(elem))
 
     answer = []
     for item in new_list:
-        if (item) in years_list:
+        if item in years_list:
             answer.append(item)
-    print(len(years_list))
-    print(len(new_list))
+
     print(f"Answer = {answer[0] * answer[1]}")
 
-#
-# def find3Numbers(A, arr_size, sum):
-#     for i in range(0, arr_size - 2):
-#         for j in range(i + 1, arr_size - 1):
-#             for k in range(j + 1, arr_size):
-#                 if A[i] + A[j] + A[k] == sum:
-#                     print("Triplet is", A[i],
-#                           ", ", A[j], ", ", A[k])
-#                     return True
-#     return False
-#
-#
-# # Driver program to test above function
-# A = years_list
-# A.sort()
-# print(A)
-# print(len(A))
-# sum = 2020
-# arr_size = len(A)
-# find3Numbers(A, arr_size, sum)
+
+def find_triplet(A, sum):
+    arr_size = len(A)
+    for i in range(0, arr_size - 2):
+        for j in range(i + 1, arr_size - 1):
+            for k in range(j + 1, arr_size):
+                if A[i] + A[j] + A[k] == sum:
+                    print(f"Three numbers that sum to {sum} are: {A[i]} ,  {A[j]} , {A[k]}")
+                    print(f"The answer to second task is =  {A[i] * A[j] * A[k]}")
+                    return True
+    return False
+
+
+find_triplet(years_list, 2020)
