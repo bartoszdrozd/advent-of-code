bf1dbf1334431956f70b49d72ec05c90e0442d31
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-12-02 16:36:45 +0100

    Task 1 improvements

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,11 +3,21 @@ import re
 
 with open(file, "r") as f:
     new_list = f.readlines()
-    print(new_list)
+    total_counter = 0
     for line in new_list:
         occurance = re.findall("[0-9]\-[0-9]*", line)
         lower_limit, upper_limit = occurance[0].split('-')
+        lower_limit = int(lower_limit)
+        upper_limit = int(upper_limit)
         letter = re.findall("([a-z]\:){1}", line)
         letter = letter[0].rstrip(":")
-        print(letter)
+        sequence = re.findall("[a-z]{2,}", line)
+        sequence = sequence[0].strip("\n")
+        for elem in sequence:
+            letter_counter = 0
+            if elem == letter:
+                letter_counter += 1
+        if lower_limit <= letter_counter and letter_counter <= upper_limit:
+            total_counter += 1
+    print(total_counter)
 
