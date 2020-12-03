eea5d56f5fdce90f0be2150a050b5c97446d47d6
Bartosz Drozd <bartosz.drozd94@gmail.com>
2020-12-03 17:59:12 +0100

    Task 1 finish

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,23 +1,21 @@
 file = "input2.txt"
 import re
 
+
 with open(file, "r") as f:
     new_list = f.readlines()
     total_counter = 0
     for line in new_list:
-        occurance = re.findall("[0-9]\-[0-9]*", line)
-        lower_limit, upper_limit = occurance[0].split('-')
+        test_string = re.match("([0-9]*\-[0-9]*)\s([a-z]\:{1})\s([a-z]{2,})", line)
+        lower_limit, upper_limit = test_string.group(1).split('-')
         lower_limit = int(lower_limit)
         upper_limit = int(upper_limit)
-        letter = re.findall("([a-z]\:){1}", line)
-        letter = letter[0].rstrip(":")
-        sequence = re.findall("[a-z]{2,}", line)
-        sequence = sequence[0].strip("\n")
+        letter = test_string.group(2).rstrip(':')
+        sequence = test_string.group(3).strip('\n')
+        letter_counter = 0
         for elem in sequence:
-            letter_counter = 0
             if elem == letter:
                 letter_counter += 1
-        if lower_limit <= letter_counter and letter_counter <= upper_limit:
+        if lower_limit <= letter_counter <= upper_limit:
             total_counter += 1
     print(total_counter)
-
