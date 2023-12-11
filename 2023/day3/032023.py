eba2514eca709d3b2411c3c2455d86d9c2a36eab
Bartosz Drozd <bartosz.drozd94@gmail.com>
2023-12-11 01:23:49 +0100

    finished day 3 first part with test cases added

--- 2023/day3/032023.py
+++ 2023/day3/032023.py
@@ -3,33 +3,30 @@ import re
 
 start_time = time.time()
 
-file = "test_input2.txt"
+file = "input.txt"
 with open(file, "r") as f:
 	data_list = f.readlines()
 characters_list = []
 characters_locations = []
 
-# data_list = ['........','.24..4..','......*.']
+def get_indices_of_char(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
 
-
-def getCharactersLocations(data_list):
+def get_characters_locations(data_list):
     line_number = 1
     for lines in data_list:
         for elem in lines:
             if elem.isalnum() == False and elem != '.' and elem != '\n':
-                characters_locations.append((line_number, lines.index(elem)))
-                # if elem not in characters_list:
-                #     characters_list.append(elem)
-                # characters_locations.append((line_number, lines.index(elem)))
+                indices = get_indices_of_char(lines, elem)
+                for index in indices:
+                    if (line_number, index) not in characters_locations:
+                        characters_locations.append((line_number, index))
+                    
         line_number += 1
-    # print(characters_list)
-    print(characters_locations)
     return characters_locations
 
-match_list = []
-
-def getNumbers(data_list):
-    print(f'length of data_list: {len(data_list)}')
+def get_numbers(data_list):
+    match_list = []
     line_number = 1
     for lines in data_list:
         matches = re.finditer(r'\d+', lines)
@@ -38,33 +35,24 @@ def getNumbers(data_list):
         line_number += 1
     return match_list
 
-def getAdjacentData(matches, char_loc):
+def get_adjacent(matches, char_loc):
     adjacent_sum = 0
     for elem in char_loc:
         for location in matches:
-            if elem[0] == 1 or location[0] == 1:
-                if elem[0] == location[0] or elem[0] == location[0] + 1:
-                    if elem[1] == location[1] - 1 or elem[1] == location[1] or elem[1] == location[1] + location[2] - 2 or elem[1] == location[1] + location[2] - 1 or elem[1] == location[1] + location[2]:
-                        adjacent_sum += int(location[3])
-                        print(f'elem: {elem}, location: {location}')
-            elif elem[0] == len(data_list) or location[0] == len(data_list):
-                if elem[0] == location[0] or elem[0] == location[0] - 1:
-                    if elem[1] == location[1] - 1 or elem[1] == location[1] or elem[1] == location[1] + location[2] - 2 or elem[1] == location[1] + location[2] - 1 or elem[1] == location[1] + location[2]:
-                        adjacent_sum += int(location[3])
-                        print(f'elem: {elem}, location: {location}')
-            elif elem[0] == location[0] or elem[0] == location[0] + 1 or elem[0] == location[0] - 1:
+            if elem[0] == location[0] or elem[0] == location[0] + 1 or elem[0] == location[0] - 1:
                 if elem[1] == location[1] - 1 or elem[1] == location[1] or elem[1] == location[1] + location[2] - 2 or elem[1] == location[1] + location[2] - 1 or elem[1] == location[1] + location[2]:
                     adjacent_sum += int(location[3])
-                    print(f'elem: {elem}, location: {location}')
     return(adjacent_sum)
 
+def gear_ratio_calc():
+    return 0
 
-# print(f'Part One: {getsum(data_list)}')
+
+get_characters_locations(data_list)
+get_numbers(data_list)
+# print(get_adjacent(get_numbers(data_list), characters_locations))
+print(f'Part One: {get_adjacent(get_numbers(data_list), characters_locations)}')
 # print(f'Part Two: {second_answer}')
-
-print(getCharactersLocations(data_list))
-print(getNumbers(data_list))
-print(getAdjacentData(match_list, characters_locations))
 
 
 end_time = time.time()
